edbe51f74ea0db4e890c95423361e5dadf1bf030
JatinJainSaraf <jatinjainsaraf@gmail.com>
2024-04-04 12:49:17 +0530

    initial commit

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -4,9 +4,9 @@ import {
   HomeIcon,
   DocumentDuplicateIcon,
 } from '@heroicons/react/24/outline';
+import clsx from 'clsx';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import clsx from 'clsx';
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
 const links = [
@@ -35,7 +35,7 @@ export default function NavLinks() {
                 'bg-sky-100 text-blue-600': pathname === link.href,
               },
             )}
-           >
+             >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
           </Link>
